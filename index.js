11761c8e8904f31b0c838f5e56316a54936bf937
NokiaZ <49866374+zyh10655@users.noreply.github.com>
2020-09-24 10:05:22 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Note = require('./models/note')
 
 const cors = require('cors')
 
@@ -106,8 +108,10 @@ let notes = [
     }
 ]
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes.map(note => note.toJSON()))
+  })
 })
 
 const generateId = () => {
@@ -120,32 +124,32 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
+  const note = new Note({
+    author: body.author,
+    title: body.title,
+    journal: body.journal,
+    year: body.year,
+    eprint: body.eprint,
+    eprinttype: body.eprinttype,
+    eprintclass: body.eprintclass,
+    pages: body.pages,
+    month: body.month,
+    annote: body.annote,
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote.toJSON())
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  Note.findById(request.params.id).then(note => {
+    response.json(note.toJSON())
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -155,7 +159,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT 
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
